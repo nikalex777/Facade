bd6edbf1f19483a69154baed57f517404a9a9070
nikalex777 <777_nikolaev@mail.ru>
2023-03-29 20:22:43 +0300

    Facade

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("100", "101"));
+        System.out.println(bins.mult("100", "111"));
+    }
+}
